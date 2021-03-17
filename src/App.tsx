cf5878d7e4436988fa1ccb3f2881523d88d3c981
Jess Perschke <anonymous171@posteo.de>
2021-03-17 16:18:47 -0400

    added color

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { HexColorPicker } from "react-colorful";
+import "react-colorful/dist/index.css";
 
 import Timer from './Timer';
 
@@ -13,11 +15,15 @@ function App() {
       
       <h2>Welcome to the Timer</h2>
 
-      {/* <Timer start={Date.now()} /> */}
       <Timer hello={name} />
 
     </div>
   );
 }
+
+let Color = () => {
+  const [color, setColor] = React.useState("#aabbcc");
+  return <HexColorPicker color={color} onChange={setColor} />;
+};
 
 export default App;
--- src/Timer.tsx
+++ src/Timer.tsx
@@ -4,54 +4,6 @@ type TimerProps = {
     hello: string;
 }
 
-// class Timer extends React.Component<TimerProps> {
-
-//     render() {
-//         const name = this.props.hello;
-//         console.log("🚀 ~ file: Timer.tsx ~ line 5 ~ Timer ~ props", name)
-//         return <p>HEY</p>
-//     }
-// }
-
-// const Timer = (props:any) => {
-//     console.log("🚀 ~ file: Timer.tsx ~ line 5 ~ Timer ~ props", props)
-//     return <p>HEY</p>
-// }
-
-
-// class Timer extends React.Component<TimerProps> {
-  
-//   start = new Date().getTime();
-
-//   state = {
-//     clock: 0
-//   }
-
-//   constructor(props:TimerProps) {
-//     super(props);
-
-//     // this.ticker = this.ticker.bind(this);
-
-//     setInterval(this.ticker, 1000);
-//   }
-
-//   ticker = () => {
-//     this.setState({clock: (new Date().getTime() - this.start)});
-//   }
-
-//   render(){
-//     return(
-//       <div>
-//         <p>You have used this timer for: </p> <br />
-//         <span>{ this.state.clock }</span>
-//         <p>seconds ago</p>
-//       </div>
-//     );
-//   }
-// }
-
-
-
 const Timer = (props: TimerProps) => {
 
   const start = new Date().getTime();
@@ -65,37 +17,9 @@ const Timer = (props: TimerProps) => {
     <div>
       <p>You have used this timer for: </p> <br />
       <span>{ clock }</span>
-      <p>seconds ago</p>
+      <p>milliseconds ago</p>
     </div>
   )
 }
-
-
-// type timerprops = {start:number};
-// class Timer extends Component<timerprops>{
-//     componentDidMount(){
-//        setInterval(this.ticker, 1000);
-//     }
-//     ticker(){
-//         this.setState({clock: new Date().getTime() - this.props.start})
-//     };
-//     constructor(props:timerprops){
-//         super(props);
-//         this.state = { 
-//             clock:0
-//         };
-//         this.ticker = this.ticker.bind(this);
-//     }
-//     render(){
-//         let clock = Math.round(this.state.clock / 1000);
-//         return(
-//             <div>
-//                 <p>You have used this timer for: </p> <br />
-//                 <span>{ clock}</span>
-//                 <p>seconds ago</p>
-//             </div>
-//         );
-//     }
-// }
 
 export default Timer;
